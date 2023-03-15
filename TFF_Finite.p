31ff02b889fa0a09d6184b8182b433b8114c4c43
Geoff Sutcliffe <geoff@cs.miami.edu>
2023-03-15 05:02:25 -0400

    Example raw files

--- TFF_Finite.p
+++ TFF_Finite.p
@@ -1,25 +1,24 @@
+%--------------------------------------------------------
 tff(human_type,type,      human: $tType ).
 tff(cat_type,type,        cat: $tType ).
 tff(jon_decl,type,        jon: human ).
 tff(garfield_decl,type,   garfield: cat ).
 tff(arlene_decl,type,     arlene: cat ).
-tff(loves_most_decl,type, loves_most: cat > cat ).
+tff(nermal_decl,type,     nermal: cat ).
+tff(loves_decl,type,      loves: cat > cat ).
 tff(owns_decl,type,       owns: ( human * cat ) > $o ).
 
-% tff(only_jon,axiom,
-%     ! [H: human] : H = jon).
-% tff(only_garfield_and_arlene,axiom,
-%     ! [C: cat] : (C = garfield | C = arlene) ).
+tff(three_cats,axiom,
+    $distinct(garfield,arlene,nermal) ).
 
-tff(garfield_not_arlene,axiom,
-    garfield != arlene ).
-
-tff(jon_owns_only_garfield,axiom,
-    ( owns(jon,garfield)
-    & ~ owns(jon,arlene) ) ).
+tff(jon_owns_garfield_not_arlene,axiom,
+    ( owns(jon,garfield) & ~ owns(jon,arlene) ) ).
 
 tff(all_cats_love_garfield,axiom,
-    ! [C: cat] : ( loves_most(C) = garfield ) ).
+    ! [C: cat] : ( loves(C) = garfield ) ).
 
 tff(jon_owns_garfields_lovers,conjecture,
-    ! [C: cat] : ( ( loves_most(C) = garfield ) => owns(jon,C) ) ).
+    ! [C: cat] :
+      ( ( loves(C) = garfield & C != arlene )
+     => owns(jon,C) ) ).
+%--------------------------------------------------------
--- TFF_Finite.s
+++ TFF_Finite.s
@@ -1,9 +1,11 @@
+%--------------------------------------------------------
 tff(human_type,type,      human: $tType ).
 tff(cat_type,type,        cat: $tType ).
 tff(jon_decl,type,        jon: human ).
 tff(garfield_decl,type,   garfield: cat ).
 tff(arlene_decl,type,     arlene: cat ).
-tff(loves_most_decl,type, loves_most: cat > cat ).
+tff(nermal_decl,type,     nermal: cat ).
+tff(loves_decl,type,      loves: cat > cat ).
 tff(owns_decl,type,       owns: ( human * cat ) > $o ).
 
 %----Types of the domains
@@ -16,8 +18,9 @@ tff(d2cat_decl,type,      d2cat: d_cat > cat ).
 tff(d_jon_decl,type,      d_jon: d_human ).
 tff(d_garfield_decl,type, d_garfield: d_cat ).
 tff(d_arlene_decl,type,   d_arlene: d_cat ).
+tff(d_nermal_decl,type,   d_nermal: d_cat ).
 
-tff(equality_lost,interpretation,
+tff(garfield,interpretation,
 %----The domain for human
     ( ( ! [H: human] : ? [DH: d_human] : H = d2human(DH)
       & ! [DH: d_human] : ( DH = d_jon )
@@ -25,21 +28,20 @@ tff(equality_lost,interpretation,
           ( d2human(DH1) = d2human(DH2) => DH1 = DH2 )
 %----The domain for cat
       & ! [C: cat] : ? [DC: d_cat] : C = d2cat(DC)
-      & ! [DC: d_cat]: ( DC = d_garfield | DC = d_arlene )
-      & $distinct(d_garfield,d_arlene)
+      & ! [DC: d_cat]: 
+        ( DC = d_garfield | DC = d_arlene | DC = d_nermal )
+      & $distinct(d_garfield,d_arlene,d_nermal)
       & ! [DC1: d_cat,DC2: d_cat] :
           ( d2cat(DC1) = d2cat(DC2) => DC1 = DC2 ) )
 %----Interpret terms and atoms
     & ( jon = d2human(d_jon)
       & garfield = d2cat(d_garfield)
       & arlene = d2cat(d_arlene)
-      & loves_most(d2cat(d_garfield)) = d2cat(d_garfield)
-      & loves_most(d2cat(d_arlene)) = d2cat(d_garfield) )
+      & nermal = d2cat(d_nermal)
+      & loves(d2cat(d_garfield)) = d2cat(d_garfield)
+      & loves(d2cat(d_arlene)) = d2cat(d_garfield)
+      & loves(d2cat(d_nermal)) = d2cat(d_garfield) )
     & ( owns(d2human(d_jon),d2cat(d_garfield))
-      & ~ owns(d2human(d_jon),d2cat(d_arlene)) ) ) ).
-
-% tff(only_jon,axiom,
-%     ! [H: human] : H = jon).
-% tff(only_garfield_and_arlene,axiom,
-%     ! [C: cat] : (C = garfield | C = arlene) ).
-
+      & ~ owns(d2human(d_jon),d2cat(d_arlene))
+      & ~ owns(d2human(d_jon),d2cat(d_nermal)) ) ) ).
+%--------------------------------------------------------
